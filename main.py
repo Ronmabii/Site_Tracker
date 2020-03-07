53cf4be1b870bc1462ac15380f979d4061738469
ronmabii <ronmabii@gmail.com>
2020-03-07 17:01:46 -0500

    setup and basic emails ready

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#woo
+import smtplib
+import credentials as cred
+
+def send_email(subject,body):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(cred.EMAIL_ADDRESS, cred.PASSWORD)
+        message = 'SUBJECT: {}\n{}'.format(subject,body)
+        server.sendmail(cred.EMAIL_ADDRESS, cred.EMAIL_ADDRESS, message) #(From, To, Message)
+        server.quit()
+        print('SUCCESS')
+    except:
+        print('FAIL')
+
+subject = 'First Test'
+body = 'Hope it works'
+
+send_email(subject,body)
+
+# typos strike again(ADRESS)
