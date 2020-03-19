a286333c01b4ec9a1d3f475230964f25bf26e376
ronmabii <ronmabii@gmail.com>
2020-03-19 15:03:30 -0400

    testing flask

--- mail.py
+++ mail.py
@@ -1,14 +1,17 @@
 import smtplib
 import credentials as cred
 
+my_email = cred.EMAIL_ADDRESS
+password = cred.PASSWORD
+
 def send_email(subject,body):
     try:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
-        server.login(cred.EMAIL_ADDRESS, cred.PASSWORD)
+        server.login(my_email, password)
         message = 'SUBJECT: {}\n{}'.format(subject,body)
-        server.sendmail(cred.EMAIL_ADDRESS, cred.EMAIL_ADDRESS, message) #(From, To, Message)
+        server.sendmail(my_email, my_email, message) #(From, To, Message)
         server.quit()
         print('SUCCESS')
     except:
